507a9c78a6f3992b227539a6a4f88c35c938c02b
Sanket Saharkar <sanketsaharkar@gmail.com>
2020-02-27 04:15:53 -0800

    modified

--- Assingment 2.py
+++ Assingment 2.py
@@ -3,26 +3,28 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-name = []
-purpose = []
 
-
-def webscraper():
+def webscraper(name, purpose):
     for i in range(50):
         resp = requests.get("http://18.207.92.139:8000/random_company")
         soup = BeautifulSoup(resp.content, "html.parser")
         item = soup.find_all("li")
+
         for i in range(len(item)):
             if item[i].get_text().split()[0] == "Name:":
                 name.append(item[i].get_text()[6:])
 
+
         for i in range(len(item)):
             if item[i].get_text().split()[0] == "Purpose:":
                 purpose.append(item[i].get_text()[9:])
-        time.sleep(1)
+
+        #time.sleep(1)
 
 
 if __name__ == "__main__":
-    webscraper()
+    name = []
+    purpose = []
+    webscraper(name, purpose)
     df = pd.DataFrame({"Name": name, "Purpose": purpose})
     df.to_csv("napu")
